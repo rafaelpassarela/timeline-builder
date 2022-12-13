1010a0a403df1a84411cfa1b793bdfb809aecf5d
rafael.passarela@kinghost.com.br <rafael.passarela@kinghost.com.br>
2022-12-13 15:59:33 -0300

    img layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+const lorem = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum like abc abc.";
+
   return (
     <div>
         <Title title='TimeLine Test' subtitle='Just a timeline builder teste component'></Title>
@@ -14,15 +17,15 @@ function App() {
                 align='right'
                 date='20/02/2022'
                 title='Novo'
-                subtitle='Outro Novo'
-                img='https://www.nicepng.com/png/detail/132-1327829_icon-free-download-and-nature-icon.png'></Event>
+                subtitle={lorem}
+                img='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz5_MSd5YCveNW5v17B2srLblp2JkuAyUXtQ&usqp=CAU'></Event>
             <Event align='left' date='14/01/2022' title='Teste' subtitle='Sub Teste'></Event>
             <Event align='right' date='20/02/2022' title='Novo' subtitle='Outro Novo'></Event>
             <Event
                 align='left'
                 date='14/01/2022'
                 title='Teste'
-                subtitle='Sub Teste'
+                subtitle={lorem}
                 img='https://cdn-icons-png.flaticon.com/512/1355/1355083.png'></Event>
             <Event align='right' date='20/02/2022' title='Novo' subtitle='Outro Novo'></Event>
         </Container>
--- src/components/EventDetailComponent.tsx
+++ src/components/EventDetailComponent.tsx
@@ -17,16 +17,18 @@ class EventDetail extends Component<EventDetailProps> {
   render() {
     const { date, title, subtitle, img, imgClass } = this.props;
     var imgTag = (img === undefined) ? null : <img className={this.getImgClassName(imgClass)} src={img} alt="Event Logo"/>;
-    var textClass = (img === undefined) ? '' : 'eventText';
+    var spacer = (imgClass === 'eventIcon-right' ) ? <div className='spacer'></div> : null;
+    //var spacer = <div className='spacer'></div>;
     return (
         <div className='eventComp'>
             {imgTag}
-            <div className={textClass}>
-                Data {date}
-                <br/>
-                Tit {title}
-                <br/>
-                Sub {subtitle}
+            <div>
+                <small>{date}</small><br/>
+                <h2>{title}</h2>
+                <div style={{textAlign: 'justify'}}>
+                    {subtitle}{spacer}
+                </div>
+
             </div>
         </div>
     );
